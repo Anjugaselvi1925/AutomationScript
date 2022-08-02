0388f3e5b12f0ba80adf77696cbd7e66b3861e95
anjugaselvi.m <anjugaselvi.m@QT-INBG-LT0642.QUALITEST.LOCAL>
2022-08-02 15:15:40 +0530

    launch successful

--- AutomationScript/src/Script/Lanuch.java
+++ AutomationScript/src/Script/Lanuch.java
@@ -7,17 +7,16 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Lanuch {
-	
 
-	    @Test 
-	    public void Launching_the_Browser()
-	    {
-	        //System.setProperty("webdriver.chrome.driver",".C:\\Users\\nishalini.mathavan\\Downloads\\chromedriver_win32.exe");
-	        WebDriverManager.chromedriver().setup();
-	        WebDriver driver = new ChromeDriver();
-	        driver.get("https://mobileworld.azurewebsites.net/");
-	        driver.manage().window().maximize();
+	@Test 
+    public void Launching_the_Browser()
+    {
+        //System.setProperty("webdriver.chrome.driver",".C:\\Users\\anjugaselvi.m\\Downloads\\chromedriver_win32.exe");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://mobileworld.azurewebsites.net/");
+        driver.manage().window().maximize();
 
-	    }
-	}
+    }
+}
 
--- AutomationScript/src/Script/loginandsignup.java
+++ AutomationScript/src/Script/loginandsignup.java
@@ -1,5 +1,13 @@
 package Script;
 
-public class loginandsignup {
+import org.openqa.selenium.By;
+
+public class loginandsignup 
+@Test
+(dependsOnMethods = {"Launching_the_Browser"})
+public void Signup_for_mobiles() throws InterruptedException
+{
+	WebElement Signin = driver.findElement(By.xpath("))
+	
 
 }
