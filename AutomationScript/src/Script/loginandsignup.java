509a486efe8622364dd296fb96f0f8e4a7346b2f
anjugaselvi.m <anjugaselvi.m@QT-INBG-LT0642.QUALITEST.LOCAL>
2022-08-02 17:48:54 +0530

    signin

--- AutomationScript/src/Script/loginandsignup.java
+++ AutomationScript/src/Script/loginandsignup.java
@@ -2,14 +2,22 @@ package Script;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import com.github.dockerjava.api.model.Driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class loginandsignup {
 @Test
 (dependsOnMethods = {"Launching_the_Browser"})
 public void Signup_for_mobiles() throws InterruptedException
 {
+	 WebDriverManager.chromedriver().setup();
+     WebDriver driver = new ChromeDriver();
 	WebElement Signin = driver.findElement(By.xpath("/button[text()='SIGN IN']"));
 	Signin.click();
 	WebElement Signup = driver.findElement(By.xpath("//a[text()='Sign up']"));
@@ -39,7 +47,7 @@ public void Signup_for_mobiles() throws InterruptedException
 	Password.sendKeys("Anju@1919");
 	driver.findElement(By.xpath("//a[text()='Log In']")).click();
 	
-	
+}
 	
 
 }
