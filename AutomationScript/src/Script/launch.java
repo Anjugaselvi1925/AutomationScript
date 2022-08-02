602fe3cc9ac0c2e212cd07ad5b18f4fa43fcf806
anjugaselvi.m <anjugaselvi.m@QT-INBG-LT0642.QUALITEST.LOCAL>
2022-08-02 11:47:33 +0530

    launch

--- AutomationScript/src/Script/launch.java
+++ AutomationScript/src/Script/launch.java
@@ -8,6 +8,6 @@ WebDriver driver;
 	System.setProperty("C:\\Users\\anjugaselvi.m\\Downloads\\chromedriver_win32.exe");
 	driver = new ChromeDriver();
 	driver.get("https://mobileworld.azurewebsites.net/");
-	driver.manage().window().maximize();
+	
 }
 }
