791141e6fcc7224f30da0e0884e6224f8b4c3618
anjugaselvi.m <anjugaselvi.m@QT-INBG-LT0642.QUALITEST.LOCAL>
2022-08-02 23:18:16 +0530

    contact

--- AutomationScript/src/contactus.java
+++ AutomationScript/src/contactus.java
@@ -1,4 +1,26 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.testng.AssertJUnit;
 
 public class contactus {
+	public class contact_page extends Launch
+	{
+		
+		@Test(priority = 4)
+		public void contactUs() throws Throwable
+		{
+			driver.findElement(By.xpath("//a[@id='navbarDropdown']")).click();
+		driver.findElement(By.linkText("Contact Us")).click();
+		Set<String> window = driver.getWindowHandles();
+		List<String> list= new ArrayList<String>(window);
+		driver.switchTo().window(list.get(1));
+		Thread.sleep(1000);
+		System.out.println(driver.getTitle());
+		WebElement ele3 = driver.findElement(By.cssSelector("h3[class='title']"));
+		AssertJUnit.assertTrue(ele3.isDisplayed());
+		
+		
+	}
 
 }
