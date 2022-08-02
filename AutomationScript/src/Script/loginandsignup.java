0fc12c7c9e24a1e90d0d8965fcf1a54aad713459
anjugaselvi.m <anjugaselvi.m@QT-INBG-LT0642.QUALITEST.LOCAL>
2022-08-02 15:29:59 +0530

    anj

--- AutomationScript/src/Script/loginandsignup.java
+++ AutomationScript/src/Script/loginandsignup.java
@@ -1,13 +1,25 @@
 package Script;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class loginandsignup 
 @Test
 (dependsOnMethods = {"Launching_the_Browser"})
 public void Signup_for_mobiles() throws InterruptedException
 {
-	WebElement Signin = driver.findElement(By.xpath("))
+	WebElement Signin = driver.findElement(By.xpath("/button[text()='SIGN IN']"));
+	Signin.click();
+	WebElement Signup = driver.findElement(By.xpath("//a[text()='Sign up']"));
+	Signup.click();
+	WebElement firstname = driver.findElement(By.xpath("//input[@id='myName']"));
+	firstname.sendKeys("Anjuga");
+	WebElement Last_name = driver.findElement(By.xpath("//input[@placeholder='Last Name']"));
+	Last_name.sendKeys("Selvi");
+	WebElement Email = driver.findElement(By.xpath("//input[@type='Email']"));
+	Email.sendKeys("anju@gmail.com");
+	WebElement password = driver.findElement(By.xpath("//input[@type='Password']"));
+	
 	
 
 }
