4a80b6870de1f351aef4ad7923e9aede9eb13f8b
anjugaselvi.m <anjugaselvi.m@QT-INBG-LT0642.QUALITEST.LOCAL>
2022-08-02 12:21:11 +0530

    launch

--- AutomationScript/src/Script/launch.java
+++ AutomationScript/src/Script/launch.java
@@ -1,13 +1,19 @@
-package Script;
+package AutomationScript;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class launch
 {
 WebDriver driver;
-@Test public void Launching_the_Browser() 
+@Test 
+public void Launching_the_Browser() 
 {
-	System.setProperty("C:\\Users\\anjugaselvi.m\\Downloads\\chromedriver_win32.exe");
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\anjugaselvi.m\\Downloads\\chromedriver_win32.exe");
 	driver = new ChromeDriver();
 	driver.get("https://mobileworld.azurewebsites.net/");
+	driver.manage().window().maximize();
 	
 }
 }
